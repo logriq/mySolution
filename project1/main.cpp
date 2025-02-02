c60d5054f5e0d2d621a0971ea3f4ab6f05146f6b
ArinaLoginova <loginova.arina@stud.onu.edu.ua>
2025-02-02 23:30:25 +0200

    third lesson

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
  // ввід/вивід інформації (5)
 
-#include <Windows.h> //для укр мови(9)
+//#include <Windows.h> для укр мови(9)
 
 int main()
 {
@@ -12,15 +12,41 @@ int main()
 
 	// /n-перенос строки
 
-	setlocale(LC_ALL, "rus"); //для переводу російської(9)
+	//setlocale(LC_ALL, "rus"); //для переводу російської(9)
 
-	SetConsoleCP(1251); // (9)встановлює кодову сторінку для вводу інформації
-	SetConsoleOutputCP(1251); // для виводу інформації (9)
+	//SetConsoleOutputCP(1251); // для виводу інформації (9)
+	//SetConsoleCP(1251); // (9)встановлює кодову сторінку для вводу інформації
 
 
-	std::cout << "Привіт, світ!\n";
-	std::cout << "Привет, мир!\n";
+	//std::cout << "Привіт, світ!\n";
+	//std::cout << "Привет, мир!\n";
+
+
+
+
+
+	/*std::cout << R"(Hello
+ World
+!)";  (10) RAW рядок: 1) ескейп послідовності не працюють. можна переносити просто переносом на наступну стор.*/
+
+	//int number;
+
+	//std::cout << "Enter a number: ";
+	//std::cin >> number;
+	//std::cout << "You entered " << number << std::endl;
+
+	int number1, number2;
+	int sum;
+
+	std::cout << "Enter first number: ";
+	std::cin >> number1;
+
+	std::cout << "Enter second number: ";
+	std::cin >> number2;
+
+	sum = number1 + number2;
+
+	std::cout << number1 << " + " << number2 << " = " << sum << std::endl;
+
 	return 0;
 }
-
-
