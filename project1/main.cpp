4ccac701b29d6283f122f59569a816c2f88974e5
ArinaLoginova <loginova.arina@stud.onu.edu.ua>
2025-02-03 15:03:56 +0200

    basic fundamentals of C++ part 2

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+//#include <iostream>
  // ввід/вивід інформації (5)
 
 //#include <Windows.h> для укр мови(9)
 
-int main()
-{
+//int main()
+//{
 	//std::cout << "Hello World!" << std::endl;  //endl-кінець строки, перенос (7)
 	//std::cout << std::endl;
 	////  (6) std-простір імен стандартний,    character output-вивід символів
@@ -35,18 +35,132 @@ int main()
 	//std::cin >> number;
 	//std::cout << "You entered " << number << std::endl;
 
-	int number1, number2;
-	int sum;
+	//(12,13)
+	// 
+	//int myAge = 23;
 
-	std::cout << "Enter first number: ";
-	std::cin >> number1;
+	//const double PI = 3.1415;
+	//int number = 3;
 
-	std::cout << "Enter second number: ";
-	std::cin >> number2;
+	//const int a = 13;
+	//const int b(13);
+	//const int v{ 13 };
+	//const int d = { 13 };
 
-	sum = number1 + number2;
 
-	std::cout << number1 << " + " << number2 << " = " << sum << std::endl;
+	//std::cout << myAge << std::endl;
+	//std::cout << "Pi + 3 = " << PI + number << std::endl;
+
+
+
+//	int number1, number2;
+//	int sum;
+//
+//	std::cout << "Enter first number: ";
+//	std::cin >> number1;
+//
+//	std::cout << "Enter second number: ";
+//	std::cin >> number2;
+//
+//	sum = number1 + number2;
+//
+//	std::cout << number1 << " + " << number2 << " = " << sum << std::endl;
+//
+//
+//
+//	return 0;
+//}
+
+
+//(15) ЛІТЕРАЛИ
+
+/*#include <iostream>
+
+int main()
+{
+	short a = 1;
+	int b =10;
+	long c = 10L;
+	long long d = 10LL;
+
+	unsigned short a = 10;
+	unsigned int b = 10U;
+	unsigned long c = 10UL;
+	unsigned long long d = 10ULL;
+
+	float a = 1.5F;
+	double b = 1.5;
+	long double c = 1.5L;
+
+	char a = 'g'; //це символьний літерал, а слово буде рядковим, його значення змінити не можна
+	wchar_t b = L'g';
+	char16_t c = u'g';
+	char32_t d = U'g';
+
+	bool a = true;
+
+	return 0;
+} */
+/*
+Блочний коментар 
+(16)
+*/
+
+#include <iostream>
+
+int main()
+{
+// = + - / % оператори присвоєння (18)
+
+	//int a, b;
+
+	//std::cout << "Enter two numbers: ";
+	//std::cin >> a >> b;
+	//
+	//a = -a;
+	//b = -b;
+
+	//std::cout << "a = " << a << std::endl;
+	//std::cout << "b = " << b << std::endl;
+
+	//std::cout << "Enter seconds: ";
+	//int userSeconds;
+	//std::cin >> userSeconds;
+
+	//const int SEC_IN_MIN = 60;
+
+	//int seconds = userSeconds % SEC_IN_MIN;
+	//int minutes = userSeconds / SEC_IN_MIN;
+
+	//std::cout << "It is " << minutes << " minutes and " << seconds << " seconds\n"; 
+
+
+
+	//std::cout << "Enter a number: ";
+	//int number;
+	//std::cin >> number;
+
+	//(21)комбіновані арифметичні оператори
+	//std::cout << "Number = " << number << std::endl;
+	//number = number + 2;
+	//std::cout << "Number = " << number << std::endl;
+	//number -= 5;
+	//std::cout << "Number = " << number << std::endl;
+	//number *= 2;
+	//std::cout << "Number = " << number << std::endl;
+	//number /= 4;
+	//std::cout << "Number = " << number << std::endl;
+	//number %= 2;
+	//std::cout << "Number = " << number << std::endl;
+
+
+
+
+	// інкремент та декремент
+	int a = 1, b = 2;
+	int c = ++a + --b;
+
+	std::cout << c << std::endl;
 
 	return 0;
 }
