c3435ef33ebd2f9cc1772092fd06ac1cc1d73273
ArinaLoginova <loginova.arina@stud.onu.edu.ua>
2025-02-04 23:27:27 +0200

    04.02 1/2

--- project1/main.cpp
+++ project1/main.cpp
@@ -169,12 +169,10 @@ int main()
 
 
 
-#include <iostream>
-
-int main()
-{
-	
-	
+//#include <iostream>
+//
+//int main()
+//{	
 	
 	// (23) ІЄРАХІЯ ТИПІВ ДАНИХ
 	/*long double
@@ -308,13 +306,108 @@ int main()
 	// 2. !false = true
 
 
-	bool expression = (!strcmp("Hello", "hello"));
+//	bool expression = (!strcmp("Hello", "hello"));
+//
+//	std::cout << expression;
+//
+//	return 0;
+//}m
 
-	std::cout << expression;
+
+#include <iostream>
+
+int main()
+{
+// Синтаксис оператора if: (28)
+
+	/*if (умова№1 )
+		дія№1
+	else
+		дія№2
+		 */
+
+	//std::cout << "Enter a number";
+	//int number;
+	//std::cin >> number;
+
+	//if (number == 1)
+	//	std::cout << "One.\n";
+	//else if (number == 2)
+	//	std::cout << "Two.\n";
+	//if (number == 3)
+	//	std::cout << "Three.\n";
 
 
 
 
+	//ТЕРНАРИЙ ОПЕРАТОР(29)
+	//?: 
+	// Умова ? Вираз1 : Вираз2
+
+	//std::cout << "Enter two numbers: ";
+	//int number1, number2;
+	//std::cin >> number1 >> number2;
+
+	//int max = (number1 > number2 ? number1 : number2);
+
+	//std::cout << max << std::endl;
+
+
+	//SWITCH(30)
+
+	/*switch(ім'я_змінної) 
+	{
+	 case Значення №1:
+			Перелік_дій №1
+			break;
+	 case Значення №2:
+			Перелік_дій №2
+			break;	
+	 case Значення №3:
+			Перелік_дій №3
+			break;	
+	 case Значення №4:
+			Перелік_дій №4
+			break;
+	 default:
+			Перелік_Дій_За_Замовчуванням
+			break;
+	 }*/
+
+
+	//ПОНЯТТЯ ЦИКЛУ
+
+	// 1. while
+	// 2. do while
+	// 3. for (стандартна форма)
+	// 4. for з діапазоном
+	
+
+
+	// Синтаскис циклу while: коли дія зв'язана з умовою
+
+	/*while (умова)
+	{
+	Перелік дій
+	}
+	*/
+
+
+	int sum = 0;
+	int number;
+
+	std::cout << "Enter a number (or 0 to stop)";
+	std::cin >> number;
+	sum += number;
+
+	while (number != 0)
+	{
+	std::cout << "Enter a number (or 0 to stop)";
+	std::cin >> number;
+	sum += number;
+	}
+
+	std::cout << "Sum = " << sum << std::endl;
 
 	return 0;
 }
