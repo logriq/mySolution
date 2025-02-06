9a50bfed108c8a913210ea8e720922a77fe247e5
ArinaLoginova <loginova.arina@stud.onu.edu.ua>
2025-02-06 23:05:02 +0200

    Pointers 06.02

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,4 +1,4 @@
-//#include <iostream>
+#include <iostream>
  // ввід/вивід інформації (5)
 
 //#include <Windows.h> для укр мови(9)
@@ -599,7 +599,7 @@ int main()
 Парамкетри            | Аргументи
 */
 
-#include <iostream>
+//#include <iostream>
 
 //int Multiple(int a, int b)
 //{
@@ -662,27 +662,452 @@ int main()
 
 // відмінність відправки масивів і змінних
 
-void A(int arr[], int size)
+//void A(int arr[], int size)
+//{
+//	arr[0] = 5;
+//}
+//
+//
+//void B(int a)
+//{
+//	a = 3;
+//}
+//
+//int main()
+//{
+//	int arr[3] = { 1, 2, 3 };
+//	int a = 10;
+//
+//	A(arr, 3);
+//	B(a);
+//
+//	std::cout << arr[0] << std::endl;
+//	std::cout << a << std::endl;
+//
+//	return 0;
+//}
+
+
+//Синтаксис створення прототипу:
+// Тип повертаючого значення  Ім'я функції(список формальнихх параметрів);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//int main() 
+//{
+	
+	// Вказівник- це зміна, яка зберігає в собі адресу.
+
+//Синтаксис створення вказівника:
+// 1. Тип_Даних *Ім'я Вказівника;
+// 2. Тип_Даних *Ім'я Вказівника * Адреса <- Ініціалізація
+
+
+// Вказівник на змінну:(можна міняти данні через вказівник)
+
+/*int a = 5;
+int b = 10;
+int* p;
+
+p = &a;
+*p = 0;
+
+std::cout << "&a = " << &a << std::endl;
+std::cout << "p = " << p << std::endl;
+std::cout << "a = " << a << std::endl;
+std::cout << "*p = " << *p << std::endl;
+
+
+p = &b;
+*p = 0;
+
+std::cout << "&b = "  << &b << std::endl;
+std::cout << "p = " << p << std::endl;
+std::cout << "b = " << b << std::endl;
+std::cout << "*p = " << *p << std::endl;*/
+
+
+
+//Вказівник на константу:(не можна міняти константу через вказівник)
+
+/*const int a = 5;
+const int b = 10;
+const int* p;
+
+p = &a;
+
+std::cout << "&a = " << &a << std::endl;
+std::cout << "p = " << p << std::endl;
+std::cout << "a = " << a << std::endl;
+std::cout << "*p = " << *p << std::endl;
+
+
+p = &b;
+
+std::cout << "&b = " << &b << std::endl;
+std::cout << "p = " << p << std::endl;
+std::cout << "b = " << b << std::endl;
+std::cout << "*p = " << *p << std::endl;*/
+
+
+
+//Вказівник на вказівник:
+
+	/*int a = 10;
+	int* p = &a;
+
+	int** pp = &p;
+
+	std::cout << "a = " << a << std::endl;
+	std::cout << "&a = " << &a << std::endl;
+	//std::cout << "*a = " << *a << std::endl;
+
+
+	std::cout << "p = " << p << std::endl;
+	std::cout << "&p = " << &p << std::endl;
+	std::cout << "*p = " << *p << std::endl;
+
+
+	std::cout << "pp = " << pp << std::endl;
+	std::cout << "&pp = " << &pp << std::endl;
+	std::cout << "*pp = " << *pp << std::endl;*/
+
+
+
+
+
+	//Вказівник на масив:
+
+	/*const int SIZE = 5;
+	int arr[SIZE] = { 1, 2, 3, 4, 5 };
+
+	int* p = arr;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = 10 * 1 + 1 * 3;
+		std::cout << arr[i] << " ";
+	}
+	std::cout << std::endl;
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		p[i] = 20 * i - 10;
+		std::cout << p[i] << " ";
+	}
+	std::cout << std::endl;
+	
+	return 0;
+}*/
+
+
+
+//Вказівник на константний масив:
+
+/*const int SIZE = 5;
+ int arr[SIZE] = { 1, 2, 3, 4, 5 };
+
+const int* p = arr;
+
+for (int i = 0; i < SIZE; i++)
 {
-	arr[0] = 5;
+	arr[i] = 10 * 1 + 1 * 3;
+	std::cout << arr[i] << " ";
+}
+std::cout << std::endl;
+
+for (int i = 0; i < SIZE; i++)
+{
+	std::cout << p[i] << " ";
+}
+std::cout << std::endl;*/
+
+
+
+
+
+
+	//Вказівник на функції:
+	/*void Func1();
+	void Func2(int a);
+	void Func3(int a, int b);
+	void Func4(int a, double b);
+	int Func5();
+	double Func6(int a);
+	char Func7(int a, int b);
+	long Func(int a, double b);
+	void Func9(int arr[], int size);
+	void Func10(const int a);
+	void Func11(const int arr[], int size);*/
+
+
+
+//Синтаксис створення вказівника:
+		// 1. Тип_Повертаючого_Значення (*Ім'я Вказівника)(Типи_Параметрів_функції);
+		// 2. Тип_Повертаючого_Значення (*Ім'я Вказівника)(Типи_Параметрів_функції) = Адреса_Функції; <- Ініціалізація
+
+	/*void (*p1)() = Func1;
+	void (*p2)(int) = Func2;
+	void (*p3)(int, int)  = Func3;
+	void (*p4)(int, double) = Func4;
+	int (*p5)() = Func5;
+	double (*p6)(int) = Func6;
+	char (*p7)(int, int) = Func7;
+	long (*p8)(int, double) = Func;
+	void (*p9)(int[], int) = Func9;
+	void (*p10)(const int) = Func10;
+	void (*p11)(const int[], int) = Func11;
+
+	Func1();
+	(*p1)();*/
+
+	//std::cout << p1 << " = " << Func1 << std::endl;
+	//std::cout << *p1 << " = " << Func1 << std::endl;
+
+	
+
+
+
+
+
+	//Константний вказівник
+
+	//int a = 10;
+	//int b = 20;
+	//const int c = 20;
+
+	//int* const p1 = &a; //Константний вказівник на змінну, прив'язується до одних даних і сам вказівник не можна мінять, але можна змінювати значення даних
+	//const int* p2 = &a; //Вказівник на константу, може вказувати на любі дані, але не можна їх міняти
+	//const int* const p1 = &a; //Константний вквзівник на консатанту(не можна змінювати данні та значення(максимальний захист)
+
+
+
+	//Нульовий вказівник
+	
+	/*int* a = nullptr */  // або 0 або NULL але nullptr краще
+
+
+
+
+
+		// void вказівник (не можна розіменовувати але можна використовувати приведення)
+
+	/*int a = 10;
+	double b = 1.2;
+
+	void* p;
+
+	p = &a;
+	std::cout << "&a = " << &a << std::endl;
+	std::cout << "p = " << p << std::endl;
+	std::cout << "*p = " << *(static_cast<int*>(p)) << std::endl;
+
+	p = &b;
+	std::cout << "&b = " << &b << std::endl;
+	std::cout << "p = " << p << std::endl;
+	std::cout << "*p = " << *(static_cast<double*>(p)) << std::endl;*/
+
+
+
+
+
+	//Арифметика вказівників
+	//додавання та віднімання=зсув вліво та вправо на певну к-сть байтів, залежно від типу даних
+
+	//const int SIZE = 5;
+	//int arr[SIZE] = { 1, 2, 3, 4, 5 };
+
+	//int* p1 = &arr[0];
+
+	//std::cout << "p1 = 0 = " << p1 + 0 << std::endl;
+	//std::cout << "&arr[0] = " << &arr[0] << std::endl;  //ці 3 строки мають однаковий розв'язок
+	//std::cout << "&p1[0] = " << &p1[0] << std::endl;
+
+
+	// Різниця вказівників = кількість комірок між ними залежно від типу даних
+
+
+
+
+
+
+	// Схожість масивів і вказівників 
+
+	/*const int SIZE = 5;
+	int arr[SIZE] = { 1, 2, 3, 4, 5 };
+	int* ptr = arr;
+
+
+
+	//Звернення до елементів значень через індекс
+	for (int i = 0; i < SIZE; i++)
+		std::cout << arr[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < SIZE; i++)
+		std::cout << ptr[i] << " ";
+	std::cout << std::endl;
+
+	//Звернення до значень елементів через розіменування
+	for (int i = 0; i < SIZE; i++)
+		std::cout << *(arr + i) << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < SIZE; i++)
+		std::cout << *(ptr + i) << " ";
+	std::cout << std::endl;
+
+
+	//Звернення до адреси елементів через індекс
+	for (int i = 0; i < SIZE; i++)
+		std::cout << &arr[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < SIZE; i++)
+		std::cout << &ptr[i] << " ";
+	std::cout << std::endl;
+
+
+	//Звернення до адреси елементів 
+	for (int i = 0; i < SIZE; i++)
+		std::cout << arr + i << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; i < SIZE; i++)
+		std::cout << ptr + i << " ";
+	std::cout << std::endl;*/
+
+//return 0;
+//}
+
+
+
+
+//Вказівникки на функції як параметри функції
+
+
+/*void Initialize(int arr[], int size, int (*initFunction)());
+void Show(const int arr[], int size, void (*showFunction)(int element));
+
+int GetVaule1_10();
+int GetVaule20_30();
+
+void ShowElement(int element);
+void ShowElementInBrackets(int element);
+void ShowElementInSqareBrackets(int element);
+
+int main()
+{
+	const int SIZE = 10;
+	int arr[SIZE];
+	
+	Initialize(arr, SIZE, GetVaule20_30);
+	Show(arr, SIZE, ShowElement);
+	Show(arr, SIZE, ShowElementInBrackets);
+	Show(arr, SIZE, ShowElementInSqareBrackets);
+
+	return 0;
 }
 
-
-void B(int a)
+void Initialize(int arr[], int size, int (*initFunction)())
 {
-	a = 3;
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = initFunction();
+	}
+}
+
+void Show(const int arr[], int size, void (*showFunction)(int element))
+{
+	for (int i = 0; i < size; i++)
+		showFunction(arr[i]);
+	std::cout << std::endl;
+}
+
+void ShowElement(int element)
+{
+	std::cout << element << " ";
+}
+
+void ShowElementInBrackets(int element)
+{
+	std::cout << "(" << element << ") ";
+}
+
+void ShowElementInSqareBrackets(int element)
+{
+	std::cout << "[" << element << "] ";
+}
+
+int GetVaule1_10()
+{
+	return rand() % 10 + 1;
+}
+
+int GetVaule20_30()
+{
+	return rand() % (30 - 10) + 20;
+}*/
+
+
+
+
+
+
+
+
+
+
+//СТЕК,КУПА,ДИНАМІЧНА ПАМ'ЯТЬ
+
+int Function2()
+{
+	int a = 5;
+	return a;
+}
+
+void Function1(int a, int b)
+{
+	double c;
+	char arr[3];
+	char* p;
 }
 
 int main()
 {
+	int variable = 10;
 	int arr[3] = { 1, 2, 3 };
-	int a = 10;
 
-	A(arr, 3);
-	B(a);
+	Function1(arr[0], variable);
 
-	std::cout << arr[0] << std::endl;
-	std::cout << a << std::endl;
+	variable = Function2();
 
 	return 0;
 }
+
+
+
